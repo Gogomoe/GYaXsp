64d8508680235722c52cf78515b6ede815dffc62
Gogo <gogo81745.moe@gmail.com>
2019-10-09 14:38:40 +0800

    fix: fix a bug that cause cannot get user after login
    Set AuthProvider to the SessionHandler.

--- src/main/kotlin/moe/gogo/verticle/APIServerVerticle.kt
+++ src/main/kotlin/moe/gogo/verticle/APIServerVerticle.kt
@@ -7,6 +7,8 @@ import io.vertx.ext.web.sstore.LocalSessionStore
 import io.vertx.kotlin.coroutines.CoroutineVerticle
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
+import moe.gogo.CoroutineService
+import moe.gogo.Service
 import moe.gogo.ServiceRegistry
 import moe.gogo.service.AuthService
 import moe.gogo.service.AuthServiceImpl
@@ -21,29 +23,35 @@ class APIServerVerticle : CoroutineVerticle() {
 
         val registry = ServiceRegistry.create(vertx, context)
 
+        restAPI = Router.router(vertx)
+
         val databaseService = DatabaseServiceImpl()
         databaseService.start(registry)
         registry[DatabaseService::class.java] = databaseService
 
-        restAPI = Router.router(vertx)
+        val authService = AuthServiceImpl(context)
+        authService.start(registry)
+        registry[AuthService::class.java] = authService
 
         restAPI.route().handler(BodyHandler.create())
-        restAPI.route().handler(SessionHandler.create(LocalSessionStore.create(vertx)))
+        restAPI.route().handler(
+            SessionHandler.create(LocalSessionStore.create(vertx)).setAuthProvider(authService.auth)
+        )
 
         restAPI.route().handler { routingContext ->
             routingContext.response().putHeader("Content-Type", "application/json")
             routingContext.next()
         }
 
-        val services = listOf(
-            AuthService::class.java to AuthServiceImpl(context)
-        )
+        authService.route(restAPI)
+
+        val services: List<Pair<Class<out Service>, CoroutineService>> = emptyList()
 
         services.forEach { (clazz, service) ->
+            registry[clazz] = service
             launch {
                 service.start(registry)
                 service.route(restAPI)
-                registry[clazz] = service
             }
         }
 
