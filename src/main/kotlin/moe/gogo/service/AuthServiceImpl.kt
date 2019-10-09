d90a67f6eb47f1a17f8b2f0fc5264ba64c4a5756
Gogo <gogo81745.moe@gmail.com>
2019-10-09 22:10:53 +0800

    refactor: extract setup sql into a sql file
    Remove setup sql from the start of service. Automatically init database may useless.

--- src/main/kotlin/moe/gogo/service/AuthServiceImpl.kt
+++ src/main/kotlin/moe/gogo/service/AuthServiceImpl.kt
@@ -10,8 +10,8 @@ import io.vertx.ext.jdbc.JDBCClient
 import io.vertx.kotlin.core.json.array
 import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.ext.auth.authenticateAwait
-import io.vertx.kotlin.ext.sql.*
-import kotlinx.coroutines.withTimeoutOrNull
+import io.vertx.kotlin.ext.sql.queryWithParamsAwait
+import io.vertx.kotlin.ext.sql.updateWithParamsAwait
 import moe.gogo.ServiceException
 import moe.gogo.ServiceRegistry
 
@@ -25,7 +25,6 @@ class AuthServiceImpl : AuthService {
     override suspend fun start(registry: ServiceRegistry) {
         dbClient = registry[DatabaseService::class.java].client()
         auth = JDBCAuth.create(registry.vertx(), dbClient)
-        createTables()
     }
 
     override fun auth(): AuthProvider = auth
@@ -85,61 +84,5 @@ class AuthServiceImpl : AuthService {
 
     private val User.username: Any?
         get() = this.principal().getString("username")
-
-    private suspend fun createTables() {
-
-        val sql = listOf(
-            """
-            CREATE TABLE IF NOT EXISTS user (
-             username VARCHAR(255) NOT NULL,
-             password VARCHAR(255) NOT NULL,
-             password_salt VARCHAR(255) NOT NULL
-            );
-            """.trimIndent(),
-            """
-            CREATE TABLE IF NOT EXISTS user_roles (
-             username VARCHAR(255) NOT NULL,
-             role VARCHAR(255) NOT NULL
-            );
-            """.trimIndent(),
-            """
-            CREATE TABLE IF NOT EXISTS roles_perms (
-             role VARCHAR(255) NOT NULL,
-             perm VARCHAR(255) NOT NULL
-            );
-            """.trimIndent(),
-            """ALTER TABLE user ADD CONSTRAINT pk_username PRIMARY KEY (username);""",
-            """ALTER TABLE user_roles ADD CONSTRAINT pk_user_roles PRIMARY KEY (username, role);""",
-            """ALTER TABLE roles_perms ADD CONSTRAINT pk_roles_perms PRIMARY KEY (role, perm);""",
-            """ALTER TABLE user_roles ADD CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES user(username);""",
-            """ALTER TABLE user_roles ADD CONSTRAINT fk_roles FOREIGN KEY (role) REFERENCES roles_perms(role);"""
-        )
-
-        withTimeoutOrNull(5000) {
-            dbClient.getConnectionAwait().use { connection ->
-                val result = connection.querySingleAwait(
-                    """
-                    SELECT COUNT(*) FROM information_schema.TABLES WHERE
-                     TABLE_NAME = 'user' or
-                     TABLE_NAME = 'user_roles' or 
-                     TABLE_NAME = 'roles_perms'
-                    """.trimIndent()
-                )!!
-                if (result.getInteger(0) == 3) {
-                    return@withTimeoutOrNull
-                } else {
-                    log.info("rebuild tables")
-                    connection.executeAwait("""DROP TABLE IF EXISTS user,user_roles,roles_perms""")
-                    sql.forEach { connection.executeAwait(it) }
-
-                    addUser("admin", "admin")
-                    val user = getUser("admin", "admin")
-                    givePermission("admin", "admin")
-                    giveRole(user, "admin")
-                }
-            }
-        } ?: throw RuntimeException("AuthService setup time out")
-
-    }
 
 }
